f959fe2787b377e8ab5c0be728f75b04b1395ea1
Naiitiry <danchukroman@gmail.com>
2025-04-15 17:36:50 +0000

    actualización admin y pago

--- ecom/payment/admin.py
+++ ecom/payment/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
 
 
 # registrar modelo en el Admin
@@ -7,3 +8,20 @@ admin.site.register(ShippingAddress)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 
+
+# Crear un OrderItem en linea
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
+    extra = 0
+
+# Extender nuestro modelo de Order
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    readonly_fields = ["date_ordered"]
+    inlines = [OrderItemInline]
+
+# Sacar del registro al modelo Order (que está arroba: admin.site.register(Order))
+admin.site.unregister(Order)
+
+# Re-registrar modelo Order y OrderAdmin
+admin.site.register(Order,OrderAdmin)
--- ecom/payment/views.py
+++ ecom/payment/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from cart.cart import Cart
 # Traemos el FORMS y MODELS del envío
 from payment.forms import ShippingForm, PaymentForm
-from payment.models import ShippingAddress
+from payment.models import ShippingAddress, Order, OrderItem
 from django.contrib import messages
+from django.contrib.auth.models import User
+from store.models import Product
 
 def payment_success(request):
     return render(request,'payment/payment_success.html',{})
@@ -34,6 +36,10 @@ def billing_info(request):
         quantities = cart.get_quants
         totals = cart.cart_total()
 
+        # Crear una sesión con Información de Envío
+        my_shipping = request.POST
+        request.session['my_shipping'] = my_shipping
+
         # Verificar si el usuario está logeado
         if request.user.is_authenticated:
             # Tomar el formulario de pago
@@ -47,4 +53,96 @@ def billing_info(request):
         return render(request,'payment/billing_info.html',{'cart_products':cart_products,'quantities':quantities,'totals':totals,'shipping_form':shipping_form})
     else:
         messages.error(request,'Access Denied!')
+        return redirect('home')
+    
+def process_order(request):
+    if request.POST:
+        # Obtenemos el carrito
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+        # tomar los datos de pago de la última página
+        payment_form = PaymentForm(request.POST or None)
+        # tomar los datos de envío de la sesión
+        my_shipping = request.session.get('my_shipping')
+
+        # Reunir información de la orden
+        full_name = my_shipping['shipping_full_name']
+        email = my_shipping['shipping_email']
+        # Crear domicilio de envío con la info de la sesión
+        shipping_address = f"{my_shipping['shipping_address1']}\n{my_shipping['shipping_address2']}\n{my_shipping['shipping_city']}\n{my_shipping['shipping_state']}\n{my_shipping['shipping_zipcode']}\n{my_shipping['shipping_country']}\n"
+        amount_paid = totals
+
+        # Creamos la orden
+        if request.user.is_authenticated:
+            # logged in
+            user = request.user
+
+            # Creamos la orden, con toda la información actualizada
+            create_order = Order(user=user,full_name=full_name,email=email,shipping_address=shipping_address,amount_paid=amount_paid)
+            create_order.save()
+
+            ### Agregar elementos de la orden - traídos de models.py OrderItem
+            # Obtener el ID de la orden
+            order_id = create_order.pk
+
+            # Obtener la información del producto
+            for product in cart_products():
+                # Obetenemos el ID del producto
+                product_id = product.id
+                # Obtenemos el precio del producto
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+                # Obtenemos Cantidad
+                for key,value in quantities().items():
+                    if int(key) == product.id:
+                        # Crear item de la orden
+                        create_order_item = OrderItem(order_id=order_id ,product_id=product_id , user=user, quantity=value, price=price)
+                        create_order_item.save()
+            # Eliminar el carrito una vez pagado
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    # Eliminar Key
+                    del request.session[key]
+
+            messages.success(request,'Listo perro')
+            return redirect('home')
+        else:
+            # Creamos la orden, con toda la información actualizada
+            create_order = Order(full_name=full_name,email=email,shipping_address=shipping_address,amount_paid=amount_paid)
+            create_order.save()
+            ### Agregar elementos de la orden - traídos de models.py OrderItem
+            # Obtener el ID de la orden
+            order_id = create_order.pk
+
+            # Obtener la información del producto
+            for product in cart_products():
+                # Obetenemos el ID del producto
+                product_id = product.id
+                # Obtenemos el precio del producto
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+                # Obtenemos Cantidad
+                for key,value in quantities().items():
+                    if int(key) == product.id:
+                        # Crear item de la orden
+                        create_order_item = OrderItem(order_id=order_id ,product_id=product_id , quantity=value, price=price)
+                        create_order_item.save()
+        
+            # Eliminar el carrito una vez pagado
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    # Eliminar Key
+                    del request.session[key]
+
+
+            messages.error(request,'No logeado')
+            return redirect('home')
+    else:
+        messages.error(request,'Access denied!')
         return redirect('home')
