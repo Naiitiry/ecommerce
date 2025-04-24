2e6ba2dd2af92c430a528298b80600f1a8a1cfe0
Naiitiry <danchukroman@gmail.com>
2025-04-24 14:38:35 +0000

    categories update

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
                 'cart.context_processors.cart',
                 'store.context_processors.categories',
                 
